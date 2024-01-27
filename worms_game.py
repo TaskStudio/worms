f3f200ae82b9bfe00a97663976254d296c04c0c3
MrSebi06 <mihneasebastian06@gmail.com>
2024-01-27 17:59:38 +0100

    Add base worm render and moving system

--- worms_game.py
+++ worms_game.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import Surface, Color
 
+from src.worm import Worm
+
 
 def main() -> None:
     pygame.init()
@@ -10,8 +12,15 @@ def main() -> None:
 
     running: bool = True
 
+    worm: Worm = Worm(position=(200, 200))
+
+    worms = pygame.sprite.Group(worm)
+
     while running:
         screen.fill(color=Color(255, 243, 230))
+
+        worms.draw(screen)
+        worms.update()
 
         # Window refresh
         pygame.display.flip()
@@ -26,6 +35,15 @@ def main() -> None:
                     case pygame.K_ESCAPE:
                         running = False
                         pygame.quit()
+                    case pygame.K_LEFT:
+                        worm.move_left()
+                    case pygame.K_RIGHT:
+                        worm.move_right()
+
+            if event.type == pygame.KEYUP:
+                match event.key:
+                    case pygame.K_LEFT | pygame.K_RIGHT:
+                        worm.stop_moving()
 
 
 main()
