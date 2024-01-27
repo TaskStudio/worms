0fa72e6672295c5a2a4c4b20dc3d754fa4fbf2e7
MrSebi06 <mihneasebastian06@gmail.com>
2024-01-27 17:59:24 +0100

    Add base worm class

--- src/worm.py
+++ src/worm.py
@@ -1,8 +1,34 @@
-from src.type_defs import Coordinates
+import pygame
+from pygame import Surface
+from pygame.math import Vector2
+from pygame.rect import Rect
+from pygame.sprite import Sprite
 
 
-class Worm:
-    def __init__(self, *, position: Coordinates | tuple[int, int]):
-        self.position = (
-            position if isinstance(position, Coordinates) else Coordinates(*position)
-        )
+class Worm(Sprite):
+    """
+    Class for the worms the players will control.
+    """
+
+    def __init__(self, *, position: tuple[int, int] = (0, 0)) -> None:
+        # Setup for the sprite class
+        super().__init__()
+        self.image: Surface = pygame.image.load("src/assets/worm.png")
+        self.rect: Rect = self.image.get_rect(center=position)
+
+        self.position: Vector2 = Vector2(position)
+        self.velocity: Vector2 = Vector2(0, 0)
+        self.speed: float = 0.5
+
+    def move_right(self) -> None:
+        self.velocity.x = 1
+
+    def move_left(self) -> None:
+        self.velocity.x = -1
+
+    def stop_moving(self) -> None:
+        self.velocity.x = 0
+
+    def update(self) -> None:
+        self.position += self.velocity * self.speed
+        self.rect.center = self.position
