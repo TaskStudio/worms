e2446c0eac091b63b718b1969075c7ee822cb880
sebastian.mihnea <sebastian.mihnea@napta.io>
2024-01-29 15:47:07 +0100

    Add multiple worms control

--- worms_game.py
+++ worms_game.py
@@ -1,26 +1,46 @@
+from queue import Queue
+
 import pygame
-from pygame import Surface, Color
+import src.globals as g
+
+from pygame import Surface, Color, Vector2
+from pygame.sprite import Group, GroupSingle
 
 from src.worm import Worm
 
 
 def main() -> None:
+    # Pygame setup
     pygame.init()
 
     pygame.display.set_caption("WORMS")
-    screen: Surface = pygame.display.set_mode((1080, 720))
+    screen: Surface = pygame.display.set_mode((g.SCREEN_WIDTH, g.SCREEN_HEIGHT))
 
     running: bool = True
 
-    worm: Worm = Worm(position=(200, 200))
+    # Worms setup
+    player_1_start_position: Vector2 = Vector2(100, g.SCREEN_HEIGHT)
+    player_2_start_position: Vector2 = Vector2(g.SCREEN_WIDTH - 100,  g.SCREEN_HEIGHT)
 
-    worms = pygame.sprite.Group(worm)
+    player_1_worms: Group[Worm] = Group([Worm(position=player_1_start_position + Vector2(i*100, 0)) for i in range(g.WORMS_PER_PLAYER)])
+    player_2_worms: Group[Worm] = Group([Worm(position=player_2_start_position - Vector2(i*100, 0)) for i in range(g.WORMS_PER_PLAYER)])
 
+    worms_group: Group[Worm] = Group([player_1_worms, player_2_worms])
+
+    worms_queue: Queue[Worm] = Queue(maxsize=len(worms_group))
+    for worms in zip(player_1_worms, player_2_worms):
+        worms_queue.put(worms[0])
+        worms_queue.put(worms[1])
+
+    current_worm: Worm = worms_queue.get()
+    worms_queue.put(current_worm)
+
+    # Game loop
     while running:
         screen.fill(color=Color(255, 243, 230))
 
-        worms.draw(screen)
-        worms.update()
+        worms_group.draw(screen)
+        worms_group.update()
 
         # Window refresh
         pygame.display.flip()
@@ -36,14 +56,18 @@ def main() -> None:
                         running = False
                         pygame.quit()
                     case pygame.K_LEFT:
-                        worm.move_left()
+                        current_worm.move_left()
                     case pygame.K_RIGHT:
-                        worm.move_right()
+                        current_worm.move_right()
+                    case pygame.K_SPACE:
+                        current_worm.stop_moving()
+                        current_worm = worms_queue.get()
+                        worms_queue.put(current_worm)
 
             if event.type == pygame.KEYUP:
                 match event.key:
                     case pygame.K_LEFT | pygame.K_RIGHT:
-                        worm.stop_moving()
+                        current_worm.stop_moving()
 
 
 main()
