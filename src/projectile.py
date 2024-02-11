c3265debfab271eab35268b7c3aa2f166168b818
MrSebi06 <mihneasebastian06@gmail.com>
2024-02-11 17:56:59 +0100

    Refacto

--- src/projectile.py
+++ src/projectile.py
@@ -1,7 +1,10 @@
-import pygame
-from pygame import Color
 import math
 from typing import Optional
+
+import pygame
+from pygame import Color
+
+import src.globals as g
 
 
 class Projectile(pygame.sprite.Sprite):
@@ -12,7 +15,9 @@ class Projectile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=start_pos)
         self.target_pos = target_pos
         self.charging_start_time: Optional[float] = None
-        self.direction = (pygame.math.Vector2(target_pos) - pygame.math.Vector2(start_pos)).normalize()
+        self.direction = (
+            pygame.math.Vector2(target_pos) - pygame.math.Vector2(start_pos)
+        ).normalize()
         self.speed = 0
         self.charging = False
         self.launched = False
@@ -21,27 +26,33 @@ class Projectile(pygame.sprite.Sprite):
         self.charging = True
         self.charging_start_time = pygame.time.get_ticks()
 
-    def stop_charging(self, max_charge_duration):
+    def stop_charging(self):
         charge_duration = pygame.time.get_ticks() - self.charging_start_time
-        normalized_charge = min(charge_duration / max_charge_duration, 1)
+        normalized_charge = min(charge_duration / g.MAX_CHARGE_DURATION, 1)
         min_speed = 1
         max_speed = 30
-        self.speed = min_speed + (max_speed - min_speed) * (normalized_charge ** 2)
+        self.speed = min_speed + (max_speed - min_speed) * (normalized_charge**2)
         self.charging = False
         self.launched = True
 
-    def update(self):
+    def update(self, screen):
+        if self.charging:
+            self.draw_charge(screen)
         if self.launched:
             self.rect.centerx += self.direction.x * self.speed
             self.rect.centery += self.direction.y * self.speed
 
-    def draw_charge(self, screen, max_charge_duration):
+    def draw_charge(self, screen):
         if self.charging:
             charge_duration = pygame.time.get_ticks() - self.charging_start_time
-            normalized_charge = min(charge_duration / max_charge_duration, 1)
+            normalized_charge = min(charge_duration / g.MAX_CHARGE_DURATION, 1)
 
             charge_color = (255 * normalized_charge, 255 * (1 - normalized_charge), 0)
             charge_angle = normalized_charge * 360
 
-            charge_rect = pygame.Rect(self.rect.centerx + 20, self.rect.centery - 70, 50, 50)
-            pygame.draw.arc(screen, charge_color, charge_rect, 0, charge_angle * (math.pi / 180), 5)
+            charge_rect = pygame.Rect(
+                self.rect.centerx + 20, self.rect.centery - 70, 50, 50
+            )
+            pygame.draw.arc(
+                screen, charge_color, charge_rect, 0, charge_angle * (math.pi / 180), 5
+            )
--- worms_game.py
+++ worms_game.py
@@ -11,112 +11,128 @@ from src.projectile import Projectile
 from src.worm import Worm
 
 
-def main() -> None:
-    # Pygame setup
-    pygame.init()
+class Game:
+    def __init__(self):
+        # Pygame setup
+        pygame.init()
 
-    pygame.display.set_caption("WORMS")
-    screen: Surface = pygame.display.set_mode((g.SCREEN_WIDTH, g.SCREEN_HEIGHT))
+        pygame.display.set_caption("WORMS")
+        self.screen: Surface = pygame.display.set_mode(
+            (g.SCREEN_WIDTH, g.SCREEN_HEIGHT)
+        )
 
-    running: bool = True
+        self.running: bool = False
 
-    # Worms setup
-    player_1_start_position: Vector2 = Vector2(100, g.SCREEN_HEIGHT)
-    player_2_start_position: Vector2 = Vector2(g.SCREEN_WIDTH - 100, g.SCREEN_HEIGHT)
+        # Worms setup
+        self.player_1_worms, self.player_2_worms = self._generate_starting_worms(
+            Vector2(100, g.SCREEN_HEIGHT),
+            Vector2(g.SCREEN_WIDTH - 100, g.SCREEN_HEIGHT),
+        )
+        self.worms_group: Group[Worm] = Group(
+            [self.player_1_worms, self.player_2_worms]
+        )  # Used for game logic and rendering
 
-    player_1_worms: Group[Worm] = Group(
-        [
-            Worm(position=player_1_start_position + Vector2(i * 100, 0))
-            for i in range(g.WORMS_PER_PLAYER)
-        ]
-    )
-    player_2_worms: Group[Worm] = Group(
-        [
-            Worm(position=player_2_start_position - Vector2(i * 100, 0))
-            for i in range(g.WORMS_PER_PLAYER)
-        ]
-    )
+        self.worms_queue: Queue[Worm] = Queue(
+            maxsize=len(self.worms_group)
+        )  # Used for turn-based logic
+        for worms in zip(self.player_1_worms, self.player_2_worms):
+            self.worms_queue.put(worms[0])
+            self.worms_queue.put(worms[1])
 
-    worms_group: Group[Worm] = Group([player_1_worms, player_2_worms])
+        self.current_worm: Worm = self.worms_queue.get()
+        self.worms_queue.put(self.current_worm)
 
-    worms_queue: Queue[Worm] = Queue(maxsize=len(worms_group))
-    for worms in zip(player_1_worms, player_2_worms):
-        worms_queue.put(worms[0])
-        worms_queue.put(worms[1])
+        # Projectiles setup
+        self.projectiles = pygame.sprite.Group()
+        self.current_projectile: Optional[Projectile] = None
 
-    current_worm: Worm = worms_queue.get()
-    worms_queue.put(current_worm)
+        # Map setup
+        self.game_map: MapElement = MapElement(
+            start_x=0, start_y=g.SCREEN_HEIGHT, width=g.SCREEN_WIDTH, height_diff=40
+        )
 
-    # Projectiles setup
-    projectiles = pygame.sprite.Group()
-    max_charge_duration: int = 3000
-    current_projectile: Optional[Projectile] = None
+    def main(self):
+        self.running = True
+        while self.running:
+            self._handle_events()
+            self.update()
 
-    # Map setup
-    map: MapElement = MapElement(start_x=0, start_y=720, width=1080, height_diff=40)
+        pygame.quit()
 
-    # Game loop
-    running: bool = True
-    while running:
+    def update(self):
+        self.screen.fill(color=Color(255, 243, 230))
+
+        self.game_map.draw(self.screen)
+
+        self.worms_group.update()
+        self.worms_group.draw(self.screen)
+
+        self.projectiles.update(self.screen)
+        self.projectiles.draw(self.screen)
+
+        # Window refresh
+        pygame.display.flip()
+
+    def _handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                self.running = False
 
             # Key down events
             if event.type == pygame.KEYDOWN:
                 match event.key:
                     case pygame.K_ESCAPE:
-                        running = False
+                        self.running = False
                         pygame.quit()
                     case pygame.K_LEFT:
-                        current_worm.move_left()
+                        self.current_worm.move_left()
                     case pygame.K_RIGHT:
-                        current_worm.move_right()
+                        self.current_worm.move_right()
                     case pygame.K_SPACE:
-                        current_worm.stop_moving()
-                        current_worm = worms_queue.get()
-                        worms_queue.put(current_worm)
+                        self.current_worm.stop_moving()
+                        self.current_worm = self.worms_queue.get()
+                        self.worms_queue.put(self.current_worm)
                     case pygame.K_r:
-                        map = MapElement(
+                        self.game_map = MapElement(
                             start_x=0, start_y=720, width=1080, height_diff=100
                         )
 
             if event.type == pygame.KEYUP:
                 match event.key:
                     case pygame.K_LEFT | pygame.K_RIGHT:
-                        current_worm.stop_moving()
+                        self.current_worm.stop_moving()
 
             # Mouse events
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if not current_projectile:
-                    current_projectile = Projectile(
-                        current_worm.rect.center, pygame.mouse.get_pos()
+                if not self.current_projectile:
+                    self.current_projectile = Projectile(
+                        self.current_worm.rect.center, pygame.mouse.get_pos()
                     )
-                    current_projectile.start_charging()
-            if event.type == pygame.MOUSEBUTTONUP and current_projectile:
-                current_projectile.stop_charging(max_charge_duration)
-                projectiles.add(current_projectile)
-                current_projectile = None
+                    self.current_projectile.start_charging()
+            if event.type == pygame.MOUSEBUTTONUP and self.current_projectile:
+                self.current_projectile.stop_charging()
+                self.projectiles.add(self.current_projectile)
+                self.current_projectile = None
 
-        if current_projectile and current_projectile.charging:
-            current_projectile.draw_charge(screen, max_charge_duration)
-
-        # Update
-        screen.fill(color=Color(255, 243, 230))
-
-        map.draw(screen)
-
-        worms_group.draw(screen)
-        worms_group.update()
-
-        projectiles.update()
-        projectiles.draw(screen)
-
-        # Window refresh
-        pygame.display.flip()
-
-    pygame.quit()
+    @staticmethod
+    def _generate_starting_worms(
+        player_1_start_position: Vector2, player_2_start_position: Vector2
+    ) -> tuple[Group, Group]:
+        player_1_worms: Group[Worm] = Group(
+            [
+                Worm(position=player_1_start_position + Vector2(i * 100, 0))
+                for i in range(g.WORMS_PER_PLAYER)
+            ]
+        )
+        player_2_worms: Group[Worm] = Group(
+            [
+                Worm(position=player_2_start_position - Vector2(i * 100, 0))
+                for i in range(g.WORMS_PER_PLAYER)
+            ]
+        )
+        return player_1_worms, player_2_worms
 
 
 if __name__ == "__main__":
-    main()
+    game = Game()
+    game.main()
