a45f59d81dfbf80da6f0a1425b721d2c317bd0c2
MrSebi06 <mihneasebastian06@gmail.com>
2024-02-11 17:06:59 +0100

    Merge dev

--- worms_game.py
+++ worms_game.py
@@ -3,11 +3,11 @@ from typing import Optional
 
 import pygame
 from pygame import Surface, Color, Vector2
-from pygame.sprite import Group, GroupSingle
+from pygame.sprite import Group
 
 import src.globals as g
-from src.worm import Worm
 from src.projectile import Projectile
+from src.worm import Worm
 
 
 def main() -> None:
@@ -21,16 +21,22 @@ def main() -> None:
 
     # Worms setup
     player_1_start_position: Vector2 = Vector2(100, g.SCREEN_HEIGHT)
-    player_2_start_position: Vector2 = Vector2(g.SCREEN_WIDTH - 100,  g.SCREEN_HEIGHT)
+    player_2_start_position: Vector2 = Vector2(g.SCREEN_WIDTH - 100, g.SCREEN_HEIGHT)
 
-    player_1_worms: Group[Worm] = Group([Worm(position=player_1_start_position + Vector2(i*100, 0)) for i in range(g.WORMS_PER_PLAYER)])
-    player_2_worms: Group[Worm] = Group([Worm(position=player_2_start_position - Vector2(i*100, 0)) for i in range(g.WORMS_PER_PLAYER)])
+    player_1_worms: Group[Worm] = Group(
+        [
+            Worm(position=player_1_start_position + Vector2(i * 100, 0))
+            for i in range(g.WORMS_PER_PLAYER)
+        ]
+    )
+    player_2_worms: Group[Worm] = Group(
+        [
+            Worm(position=player_2_start_position - Vector2(i * 100, 0))
+            for i in range(g.WORMS_PER_PLAYER)
+        ]
+    )
 
     worms_group: Group[Worm] = Group([player_1_worms, player_2_worms])
-    
-    projectiles = pygame.sprite.Group()
-    max_charge_duration: int = 3000
-    current_projectile: Optional[Projectile] = None
 
     worms_queue: Queue[Worm] = Queue(maxsize=len(worms_group))
     for worms in zip(player_1_worms, player_2_worms):
@@ -39,6 +45,11 @@ def main() -> None:
 
     current_worm: Worm = worms_queue.get()
     worms_queue.put(current_worm)
+
+    # Projectiles setup
+    projectiles = pygame.sprite.Group()
+    max_charge_duration: int = 3000
+    current_projectile: Optional[Projectile] = None
 
     # Game loop
     while running:
@@ -53,6 +64,8 @@ def main() -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+            # Key down events
             if event.type == pygame.KEYDOWN:
                 match event.key:
                     case pygame.K_ESCAPE:
@@ -66,13 +79,18 @@ def main() -> None:
                         current_worm.stop_moving()
                         current_worm = worms_queue.get()
                         worms_queue.put(current_worm)
-
+            # Key up events
             if event.type == pygame.KEYUP:
-                if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                    worm.stop_moving()
+                match event.key:
+                    case pygame.K_LEFT | pygame.K_RIGHT:
+                        current_worm.stop_moving()
+
+            # Mouse events
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if not current_projectile:
-                    current_projectile = Projectile(worm.rect.center, pygame.mouse.get_pos())
+                    current_projectile = Projectile(
+                        current_worm.rect.center, pygame.mouse.get_pos()
+                    )
                     current_projectile.start_charging()
             if event.type == pygame.MOUSEBUTTONUP and current_projectile:
                 current_projectile.stop_charging(max_charge_duration)
@@ -81,9 +99,6 @@ def main() -> None:
 
         worms.update()
         worms.draw(screen)
-                match event.key:
-                    case pygame.K_LEFT | pygame.K_RIGHT:
-                        current_worm.stop_moving()
 
         if current_projectile and current_projectile.charging:
             current_projectile.draw_charge(screen, max_charge_duration)
