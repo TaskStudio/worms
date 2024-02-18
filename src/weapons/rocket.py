d541cb8f7fdbe5e92d29741a4ee271bd374537bb
Teddy <102239127+Onsraa@users.noreply.github.com>
2024-02-18 23:34:17 +0100

    Add image

--- src/weapons/rocket.py
+++ src/weapons/rocket.py
@@ -4,3 +4,7 @@ from src.weapons import Projectile
 class Rocket(Projectile):
     def __init__(self):
         super().__init__()
+        self.image = pygame.image.load('src/assets/Sainte_roquette.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
+
