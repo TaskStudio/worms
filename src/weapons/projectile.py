42483d8d752a7ac6dea7bdbbcf1a9c96687344b5
MrSebi06 <mihneasebastian06@gmail.com>
2024-02-18 16:07:14 +0100

    Projectile works with Rigidbody !

--- src/weapons/projectile.py
+++ src/weapons/projectile.py
@@ -5,41 +5,35 @@ from pygame import Color, Surface, Vector2
 from pygame.sprite import Group, Sprite
 
 import src.globals as g
+from src.physics import Rigidbody
 from src.timer import Timer
 
 
-class Projectile(Sprite):
+class Projectile(Sprite, Rigidbody):
     def __init__(self):
-        super().__init__()
+        Sprite.__init__(self)
+        Rigidbody.__init__(self, mass=1)
+
         self.image = pygame.Surface((5, 5))
         self.image.fill(Color(255, 0, 0))
         self.rect = self.image.get_rect()
-        self.start_pos = Vector2()
+
         self.target_pos = Vector2()
+
         self.charging_start_time = None
-        self.velocity = pygame.math.Vector2(0, 0)
         self.charging = False
         self.launched = False
-        self.gravity = 980  # Gravité ajustée à l'échelle du jeu (en pixels/s^2)
+
         self.launch_time = 0
-        self.scale = 100  # 1 mètre = 100 pixels
         self.charge_timer = Timer()
         self.destroyed = False
 
     def set_position(self, position: Vector2):
         self.start_pos = position
-        self.rect.center = position
+        self.position = position
 
     def set_target(self, target: Vector2):
         self.target_pos = target
-
-    def calculate_initial_velocity(self, angle, speed):
-        # Convertir la vitesse en pixels/s, ajustée par l'échelle
-        speed_pixels = speed * self.scale
-        self.velocity.x = math.cos(angle) * speed_pixels
-        self.velocity.y = (
-            -math.sin(angle) * speed_pixels
-        )  # Négatif car l'axe Y est inversé dans Pygame
 
     def start_charging(self):
         self.charging = True
@@ -49,18 +43,11 @@ class Projectile(Sprite):
         if self.charge_timer.get_seconds() == 0:
             return
 
-        normalized_charge = min(
-            self.charge_timer.get_seconds() / g.MAX_CHARGE_DURATION, 1
-        )
-        min_speed = 1  # m/s
-        max_speed = 30  # m/s
-        self.speed = min_speed + (max_speed - min_speed) * normalized_charge
-        angle = math.atan2(
-            -(self.target_pos.y - self.start_pos.y),
-            self.target_pos.x - self.start_pos.x,
-        )  # Angle ajusté
-        self.calculate_initial_velocity(angle, self.speed)
-        self.launch_time = self.charge_timer.get_seconds()
+        magnitude = self.charge_timer.get_seconds() * g.WORMS_STRENGTH
+        direction = self.target_pos - self.start_pos
+        direction.scale_to_length(magnitude * g.m)
+        self.apply_forces(direction)
+
         self.charging = False
         self.launched = True
 
@@ -78,20 +65,11 @@ class Projectile(Sprite):
         super().kill()
 
     def update(self):
+        self.rect.center = self.position
         if self.charging:
             self.charge_timer.update()
         if self.launched:
             self.charge_timer.update()
-            time_elapsed = (
-                self.charge_timer.get_seconds() - self.launch_time
-            )  # Convertir en secondes
-            # Mise à jour de la position avec la formule de la physique
-            displacement_x = self.velocity.x * time_elapsed
-            displacement_y = (
-                self.velocity.y * time_elapsed + 0.5 * self.gravity * time_elapsed**2
-            )
-            self.rect.x = self.start_pos.x + displacement_x
-            self.rect.y = self.start_pos.y + displacement_y
 
             if self.rect.y > g.SCREEN_HEIGHT:
                 self.launched = False
