9bc750f2fcef49519e3d3046388273e19ab22431
MrSebi06 <mihneasebastian06@gmail.com>
2024-02-18 16:02:22 +0100

    Fix Rigidbody trajectory calc

--- src/physics/rigidbody.py
+++ src/physics/rigidbody.py
@@ -1,3 +1,5 @@
+import math
+
 from pygame import Vector2
 
 import src.globals as g
@@ -6,24 +8,35 @@ import src.globals as g
 class Rigidbody:
     def __init__(self, mass=1, position=Vector2(0, 0), velocity=Vector2(0, 0)):
         self.mass = mass
-        self.gravity = g.GRAVITY
+        self.gravity = g.g
         self.position = position
-        self.velocity = velocity
-        self.forces = Vector2(0, 0)
+        self.initial_velocity = velocity
+        self.start_pos = Vector2()
+        self.time = 0
 
-    def add_force(self, force):
-        self.forces += force / self.mass
+    def apply_forces(self, force):
+        self.start_pos = self.position.copy()
+        self.time = 0
+
+        angle = math.radians(force.angle_to(Vector2(1, 0)))
+        self.initial_velocity.x = force.magnitude() * math.cos(angle)
+        self.initial_velocity.y = -force.magnitude() * math.sin(angle)
 
     def set_velocity(self, velocity):
-        self.velocity = velocity / self.mass
+        self.initial_velocity = velocity
 
     def clear_horizontal_velocity(self):
-        self.velocity.x = 0
+        self.initial_velocity.x = 0
 
     def physics_update(self, delta_time):
-        # Apply gravity as a constant force
-        self.add_force(Vector2(0, self.mass * self.gravity))
-        # Update velocity with accumulated forces
-        self.velocity += self.forces * delta_time
-        # Update position with velocity
-        self.position += self.velocity * delta_time
+        # if self.position.y >= g.SCREEN_HEIGHT - 100:
+        #     self.velocity.y = 0
+
+        delta_time /= 1000
+        self.time += delta_time
+        self.position.x = self.initial_velocity.x * self.time + self.start_pos.x
+        self.position.y = (
+            self.initial_velocity.y * self.time
+            + self.start_pos.y
+            + (0.5 * self.gravity * self.time**2)
+        )
