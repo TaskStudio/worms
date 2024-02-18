143afb7eb03efb5f390f2113edc71d4e5281512c
kamil TEBANI <kamiltebn@gmail.com>
2024-02-18 07:19:33 +0100

    added projectile sprite

--- src/weapons/grenade.py
+++ src/weapons/grenade.py
@@ -1,7 +1,13 @@
-from src.weapons.projectile import Projectile  # Assure-toi que le chemin d'importation est correct
+import pygame
+
+from src.weapons import Projectile
 
 
 class Grenade(Projectile):
     def __init__(self):
         super().__init__()
         self.explosion_radius = 50  # Pixels
+        # Load and set the grenade image
+        self.image = pygame.image.load('src/assets/W4_Grenade.webp').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
--- src/weapons/rocket.py
+++ src/weapons/rocket.py
@@ -7,12 +7,15 @@ class Rocket(Projectile):
     def __init__(self, wind=(0,0)):
         super().__init__()
         self.wind = pygame.math.Vector2(wind)
+        # Load and set the rocket image
+        self.image = pygame.image.load('src/assets/W4_Dynamite.webp').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect()
 
     def update(self):
         super().update()
         if self.launched:
             time_elapsed = (pygame.time.get_ticks() - self.launch_time) / 1000.0
-
             self.velocity.x += self.wind.x * time_elapsed
             self.velocity.y += self.wind.y * time_elapsed
 
