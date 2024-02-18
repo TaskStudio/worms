ae9b56301b1d676eedaff493c11beb66dc9f374c
MrSebi06 <mihneasebastian06@gmail.com>
2024-02-18 16:05:29 +0100

    Adjust grenade init

--- src/weapons/grenade.py
+++ src/weapons/grenade.py
@@ -1,7 +1,11 @@
-from src.weapons.projectile import Projectile  # Assure-toi que le chemin d'importation est correct
+from src.physics import Rigidbody
+from src.weapons.projectile import (
+    Projectile,
+)
 
 
 class Grenade(Projectile):
     def __init__(self):
         super().__init__()
+        Rigidbody.__init__(self, mass=1)
         self.explosion_radius = 50  # Pixels
