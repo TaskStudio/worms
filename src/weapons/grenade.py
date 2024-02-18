00b4c09dd5c9e216c4d20ba596cca4e3c6b5bf84
Teddy <102239127+Onsraa@users.noreply.github.com>
2024-02-18 23:33:22 +0100

    Scale new image

--- src/weapons/grenade.py
+++ src/weapons/grenade.py
@@ -5,3 +5,6 @@ class Grenade(Projectile):
     def __init__(self):
         super().__init__()
         self.explosion_radius = 50  # Pixels
+        self.image = pygame.image.load('src/assets/W4_Grenade.webp').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
